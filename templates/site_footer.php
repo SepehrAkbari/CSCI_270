42b34047ec57ef8141aabd297a9b60f58fac752c
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-03-06 11:03:45 -0600

    Update and rename lists.html to site_footer.php

---
+++ templates/site_footer.php
@@ -0,0 +1,16 @@
+            </div>
+            <hr class="footer_hr">
+            <div id="footer">
+                <div class="col33">
+                    <p class="cc-l"><a href="http://www.lakeforest.edu" target="_blank" class="drctLnk">lakeforest.edu</a></p>
+                </div>
+                <div class="col33">
+                    <p class="cc-c">© 2024 <a href="http://csci.lakeforest.edu/~akbaris79/" target="_blank" class="special_a">Sepehr Akbari</a></p>
+                </div>
+                <div class="col33">
+                    <p class="cc-r"><a href="#top" class="drctLnk">Back To Top</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
