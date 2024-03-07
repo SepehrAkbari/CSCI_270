71005927bb61f45cd746653d6fad6ec49e37ff02
Sepehr Akbari <82570094+SepehrAkbari@users.noreply.github.com>
2024-03-07 13:58:08 -0600

    Update site_footer.php

--- templates/site_footer.php
+++ templates/site_footer.php
@@ -5,7 +5,7 @@
                     <p class="cc-l"><a href="http://www.lakeforest.edu" target="_blank" class="drctLnk">lakeforest.edu</a></p>
                 </div>
                 <div class="col33">
-                    <p class="cc-c">© 2024 <a href="http://csci.lakeforest.edu/~akbaris79/" target="_blank" class="special_a">Sepehr Akbari</a></p>
+                    <p class="cc-c">© 2024 <a href="/~akbaris79/" target="_blank" class="special_a">Sepehr Akbari</a></p>
                 </div>
                 <div class="col33">
                     <p class="cc-r"><a href="#top" class="drctLnk">Back To Top</a></p>
